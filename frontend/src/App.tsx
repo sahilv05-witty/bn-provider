e2e5758dd767195cda217bf1583f4c82b21fe32a
moin <moin.shifu@wittybrains.com>
2022-12-01 12:56:36 +0530

    BET: 4910:Created page for Term of Use

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,24 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import Login from './pages/Login/Login';
-import AccountActivation from './pages/AccountActivation';
-import CreateAdminUser from './pages/CreateAdminUser';
-import CreateProviderUser from './pages/CreateProviderUser';
-import { PatientStatus } from './pages/PatientStatus/PatientStatus';
-import './App.scss';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import AccountActivation from "./pages/AccountActivation";
+import CreateAdminUser from "./pages/CreateAdminUser";
+import CreateProviderUser from "./pages/CreateProviderUser";
+import { PatientStatus } from "./pages/PatientStatus/PatientStatus";
+import "./App.scss";
+import { TermOfUse } from "./pages/TermOfUse/TermOfUse";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route  element={<PatientStatus />} path="/patient-status" />
-        <Route  element={<AccountActivation />} path="/account-activation" />
+        <Route element={<TermOfUse />} path="/term-of-use" />
+        <Route element={<PatientStatus />} path="/patient-status" />
+        <Route element={<AccountActivation />} path="/account-activation" />
         <Route element={<CreateAdminUser />} path="/create-admin-user" />
         <Route element={<CreateProviderUser />} path="/create-provider-user" />
-        <Route element={<Login />} path="/login"  />
-        <Route element={<Login />} path="/"  />
-        <Route element={<Login />} path="*"  />
+        <Route element={<Login />} path="/login" />
+        <Route element={<Login />} path="/" />
+        <Route element={<Login />} path="*" />
       </Routes>
     </Router>
   );
--- frontend/src/controls/sharedComponents/ProviderHeader/ProviderHeader.tsx
+++ frontend/src/controls/sharedComponents/ProviderHeader/ProviderHeader.tsx
@@ -1,4 +1,4 @@
-import React, { Provider } from "react";
+import React, { useState } from "react";
 import { Image, Item, Header } from "semantic-ui-react";
 import logo from "../../../assets/img/bn-logo.png";
 import "./ProviderHeader.scss";
@@ -8,11 +8,15 @@ interface ProviderHeader {
 }
 
 export const ProviderHeader = ({ toggleMenu }: ProviderHeader) => {
+  const [isActive, setActive] = useState(false);
+  const toggleSidebar = () => {
+    setActive(!isActive);
+  };
   return (
     <Item as="div" className="provider-header">
       {toggleMenu && (
         <Item as="div" className="header-content">
-          <Item as="div" className="toggle-btn" onClick={toggleMenu}>
+          <Item as="div" className="toggle-btn" onClick={toggleSidebar}>
             <Item as="span"></Item>
             <Item as="span"></Item>
             <Item as="span"></Item>
